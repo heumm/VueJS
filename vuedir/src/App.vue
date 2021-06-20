63a032c0150bfaa80e03a51883f123712757e32a
heumm <hanheum97@gmail.com>
2021-06-21 00:49:13 +0900

    room images added & products data refactor

--- vuedir/src/App.vue
+++ vuedir/src/App.vue
@@ -6,7 +6,8 @@
   </div>
 
   <div v-for="(product,i) in products" :key="i">
-    <h4>{{product}}</h4>
+    <img :src="product.image" class="room-img">
+    <h4>{{product.roomname}}</h4>
     <p>50 만원</p>
     <button v-on:click="increase(i)">허위매물신고</button> <span>신고수 : {{ accusecount[i] }}</span>
     <!-- v-on:click 또는 @click으로도 가능 --> 
@@ -23,7 +24,21 @@ export default {
     return{
       accusecount : [0, 0, 0],
       menu : ['Home', 'Shop', 'About'],
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      products : [
+          {
+            roomname : '역삼동원룸',
+            image : require('./assets/room0.jpg')
+          },
+          {
+            roomname : '천호동원룸',
+            image : require('./assets/room1.jpg')
+          },
+          {
+            roomname : '마포구원룸',
+            image : require('./assets/room2.jpg')
+          },
+        ], 
+      
     }
   },
   methods: {
@@ -43,6 +58,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 
 .menu {

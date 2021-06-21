45d8b9fe80de6ce91585d27f8c30fd1639a56f35
heumm <hanheum97@gmail.com>
2021-06-21 13:25:52 +0900

    import room data

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -6,16 +6,15 @@
       <p>상세페이지 내용</p>
     </div>
   </div>
-
   <div class="menu">
     <a v-for="a, in menu" :key="a">{{a}}</a>
     <!-- key는 반복문 돌린 요소를 컴퓨터가 구분하기 위해 쓴다. -->
   </div>
 
-  <div v-for="(product,i) in products" :key="i">
-    <img :src="product.image" @click="modalIsOpen = true" class="room-img">
-    <h4>{{product.roomName}}</h4>
-    <p>50 만원</p>
+  <div v-for="(product,i) in rooms" :key="i">
+    <img :src="rooms[i].image" @click="modalIsOpen = true" class="room-img">
+    <h4>{{rooms[i].title}}</h4>
+    <p>{{rooms[i].price}} 원</p>
     <button v-on:click="increase(i)">허위매물신고</button> <span>신고수 : {{ accuseCount[i] }}</span>
     <!-- v-on:click 또는 @click으로도 가능 --> 
   </div>
@@ -25,11 +24,14 @@
 
 <script>
 
+import roomData from './assets/oneroom.js'
+
 export default {
   name: 'App',
   data(){
     return{  
       //react에서는 state라고도 부른다.
+      rooms : roomData,
       modalIsOpen : false,
       accuseCount : [0, 0, 0],
       menu : ['Home', 'Shop', 'About'],
